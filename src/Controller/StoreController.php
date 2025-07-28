6394ef80ab9b70d84ae186e4f3495c0b50ff5ab3
zyablik <nevinny@gmail.com>
2025-07-28 14:06:32 +0300

    clear comments

--- src/Controller/StoreController.php
+++ src/Controller/StoreController.php
@@ -19,16 +19,9 @@ class StoreController extends AbstractController
         $main = $request->attributes->get('main');
         $template = $request->attributes->get('template');
         $context['page'] = $this->repository->findOneBy(['id' => $main->getEntityId()]);
-//        $context['list'] = $this->storeRepository->findBy([
-//            'parent' => $main->getId(),
-//            'status' => Statuses::Active,
-//        ]);
-//        dd($main,$context);
         return $this->render($template, [
             'main' => $main,
             'page' => $context['page'],
-//            'list' => $context['list'],
-//            'form' => $form->createView()
         ]);
     }
 }
